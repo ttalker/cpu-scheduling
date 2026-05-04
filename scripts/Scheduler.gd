8664409a31a180f6b0f536336f70064e1984c44e
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-05 02:04:08 +0800

    added static func that provides clean copy each process

--- scripts/Scheduler.gd
+++ scripts/Scheduler.gd
@@ -1,1 +1,10 @@
-extends Node
+class_name Scheduler
+
+# used a class so you just call Scheduler.method name 
+
+static func _clean_copy(processes: Array) -> Array:
+	var clean_copy = [] # where the new copy will be stored
+	for p in processes:
+		var new_process = Process.new(p.pid, p.arrival_time, p.burst_time, p.priority)
+		clean_copy.append(new_process)
+	return clean_copy
