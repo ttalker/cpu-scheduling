9beca0de0e15f1f02811e6942613e63835daba87
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-04 14:22:00 +0800

    finished the Process class

--- scripts/Process.gd
+++ scripts/Process.gd
@@ -1,9 +1,31 @@
-extends Node
-
 class_name Process
 
-var pid: String
+var pid: String # p1, p2  
 var arrival_time: int
 var burst_time: int
 var priority: int
+
+# to be computed by the scheduler
+var completion_time: int
 var remaining_time: int  
+var start_time: int      
+var turnaround_time: int
+var waiting_time: int
+
+func _init(p: String, at: int, bt: int, pri: int = 0) -> void:
+	pid = p
+	arrival_time = at
+	burst_time = bt
+	priority = pri # default is 0
+	remaining_time = bt  
+	completion_time = -1 # -1 represents not completed or not started
+	start_time = -1
+
+func compute_stats() -> void:
+	turnaround_time = completion_time - arrival_time
+	waiting_time    = turnaround_time - burst_time
+
+func reset() -> void:
+	remaining_time = burst_time   
+	completion_time = -1           
+	start_time = -1           
