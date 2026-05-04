c8d948984665d25cbc6ac1ca8eafe4d93196e1a9
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-04 13:55:39 +0800

    added main menu scene and script
    simple UI with simulate button and quit button, connected the button press

---
+++ scenes/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_button_pressed() -> void:
+	print("Pressed the simulate button")
+
+
+func _on_button_2_pressed() -> void:
+	print("Pressed the quit button")
--- scripts/Process.gd
+++ scripts/Process.gd
@@ -1,1 +1,9 @@
 extends Node
+
+class_name Process
+
+var pid: String
+var arrival_time: int
+var burst_time: int
+var priority: int
+var remaining_time: int  
