c33aa19371fc3475639612342ef023ec4350fc4a
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-11 16:29:57 +0800

    added functionality to the buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,5 +6,6 @@ func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/simulation_menu.tscn")
 
 
-func _on_button_2_pressed() -> void:
-	print("Pressed the quit button")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
