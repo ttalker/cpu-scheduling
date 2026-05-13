cc584e1201f8a96fe3f1c8a52ecdc0e9f33be744
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-13 16:51:20 +0800

    done w process row

---
+++ scenes/process_row.gd
@@ -0,0 +1,30 @@
+extends Control
+
+signal row_deleted
+
+@onready var id_label       = $IDLabel
+@onready var arrival_input  = $ArrivalInput
+@onready var burst_input    = $BurstInput
+@onready var priority_input = $PriorityInput
+@onready var delete_btn     = $DeleteBtn
+
+func _ready():
+	delete_btn.pressed.connect(_on_delete_pressed)
+
+func set_id(n: int):
+	id_label.text = "P" + str(n)
+
+func show_priority(value: bool):
+	priority_input.visible = value
+
+func get_data() -> Dictionary:
+	return {
+		"id":       id_label.text,
+		"arrival":  arrival_input.text.to_int(),
+		"burst":    burst_input.text.to_int(),
+		"priority": priority_input.value if priority_input.visible else 0
+	}
+
+func _on_delete_pressed():
+	row_deleted.emit()
+	queue_free()
